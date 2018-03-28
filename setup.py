563d456db64f3b74f4f6113e3333a4b2c3602bce
Ellis Percival <flyte@failcode.co.uk>
2018-03-28 14:52:00 +0000

    0.1.6 -> 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='confp',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     install_requires=[
